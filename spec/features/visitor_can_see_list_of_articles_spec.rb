e931e3d0ba065d8a30d9b413a509d26a1931143d
swardbladpaulo <paulo@swardblads.se>
2020-11-09 15:29:17 +0100

    created first and second spec

---
+++ spec/features/visitor_can_see_list_of_articles_spec.rb
@@ -0,0 +1,15 @@
+feature 'list articles on index page' do
+  context 'with articles in db' do
+    before do
+      visit root_path
+    end
+
+    it 'displays first article title' do
+      expect(page).to have_content 'A breaking news item'
+    end
+
+    it 'displays the second articles' do
+      expect(page).to have_content 'Some really breaking action'
+    end
+  end
+end
