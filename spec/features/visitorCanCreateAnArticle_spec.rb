aa1f865a7fc7e2bdcef1210d2b8290e250c380e2
swardbladpaulo <paulo@swardblads.se>
2020-11-14 15:47:21 +0100

    creating new specfile visitorCanCreateAnArticle

---
+++ spec/features/visitorCanCreateAnArticle_spec.rb
@@ -0,0 +1,26 @@
+feature 'Visitor can write an article' do
+  before do
+    visit root_path
+    click_on 'Write an Article'
+  end
+
+  context 'by providing all necessary information' do
+    before do
+      fill_in 'Title', with: 'Another greate demo from Thomas'
+      fill_in 'Content', with: 'Learning this stuff is alright'
+      click_on 'Create Article'
+    end
+
+    it 'is expected to save a new article in the database' do
+      expect(Article.count).to eq 1      
+    end
+
+    it 'is expected to respond with a flash message' do
+      expect(page).to have_content 'Your article was sucessfully created'
+    end
+
+    it 'is expected to re-route user to applications root path' do
+      expect(current_path).to eq root_path
+    end
+  end
+end
